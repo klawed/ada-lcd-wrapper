11789c8731a2733b4f2595ce1c85b400eacd7b8d
Claude Keswani <ckeswani@Macintosh.local>
2015-10-24 11:14:20 -0400

    updating documentation and readme

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # lcd-wrapper
-This is a wrapper for [this excellent library](https://github.com/fehmer/adafruit-i2c-lcd). 
+This repo composes the lcd object provided by [this excellent library](https://github.com/fehmer/adafruit-i2c-lcd).
 
 ## Usage
+```
+//code here REALLY soon
+```
+### function initialize()
+initializes the board or throws an error as returned by the i2c subsystem
+### function staticMessage(message)
+params: message
+* clear screen and send message to screen.  `message` will be truncated if it is longer than 16 characters
+### function displayMenu(list,property)
+display a list of menu items associated with actions  that are invoked when the select button is pressed
 
 ## TODO
-
+### Unit Tests
+### Mock LCD/LCD Simulator
--- lcd.js
+++ lcd.js
@@ -1,85 +1,106 @@
-require('coffee-script');
-require('coffee-script/register');
-require('i2c');
-var Promise = require('promise');
-var LCDPLATE=require('adafruit-i2c-lcd').plate;
-var SELECT = 1;
-var WEST = 2;
-var NORTH = 8;
-var EAST = 16;
-var SOUTH = 4;
+  require('coffee-script');
+  require('coffee-script/register');
+  require('i2c');
+  var Promise = require('promise');
+  var LCDPLATE=require('adafruit-i2c-lcd').plate;
+  var SELECT = 1;
+  var WEST = 2;
+  var NORTH = 8;
+  var EAST = 16;
+  var SOUTH = 4;
 
-function initialize() {
-    lcd= new LCDPLATE( '/dev/i2c-1', 0x20);
-    lcd.backlight(7);
-    staticMessage('Scan an item\nor press select');
-}
+  /*
+  hardcoded to i2c address of the board from (this kit.)[https://www.adafruit.com/products/1110]
+  */
+  function initialize() {
+      lcd= new LCDPLATE( '/dev/i2c-1', 0x20);
+      lcd.backlight(7);
+      staticMessage('Scan an item\nor press select');
+  }
 
-function logButton(button) {
-    console.log(button);
-}
+  function logButton(button) {
+      console.log(button);
+  }
 
-function displayMessage(message) {
-    if (message.length > 15) {
-	scrollMessage(message);
-    } else {
-	staticMessage(message);
-    }
-}
+/*
+ * params: string message
+ * prints message as static if shorter than 16 characters,
+ * else, scrolls message
+ */
+  function displayMessage(message) {
+      if (message.length > 15) {
+  	     scrollMessage(message);
+      } else {
+  	     staticMessage(message);
+      }
+  }
 
-function staticMessage(message) {
-	lcd.clear();
-	lcd.message(message);
-}
-var scroller;
-function scrollMessage(message) {
-    var screenLen = 16;
-    clearInterval(scroller);
-    var messageLen = message.length;
-    var i = 0;
-    scroller = setInterval(function(){
-	var substr;
-	if (i < messageLen - screenLen) {
-	    substr = message.substr(i, screenLen);
-	} else if (i < messageLen) {
-	    substr = message.substr(i, messageLen - i) + " " + message.substr(0,(screenLen - (messageLen - i -1)));
-	} else {
-	    i = 0;
-	    substr = message.substr(i, screenLen);
-	}
-	lcd.clear();
-	lcd.message(substr);
-	i++;
-    },400);
-}
-function displayMenu(list,property) {
-    var promise = new Promise(function(resolve, reject) {
-	var curIndex = 0;
-	var total = list.length;
-	lcd.clear();
-	staticMessage((curIndex +1) + " of " + total + "\n* " + list[curIndex][property].substr(0,13));
-	var selectMenu = function(button) {
-	    switch(button) {
-	    case SELECT:
-		lcd.removeListener("button_up", arguments.callee);
-		resolve(list[curIndex]);
-		break;
-	    case SOUTH:
-		if (curIndex < list.length-1)
-		    curIndex++;
-		break;
-	    case NORTH:
-		if (curIndex > 0)
-		    curIndex--;
-		break
-	    }
-	    lcd.clear();
-	    staticMessage((curIndex +1) + " of " + total + "\n* " + list[curIndex][property].substr(0,13));
-	}
-	lcd.on('button_up',selectMenu);
-    });
-    return promise;
-}
+/*
+ * params: string message
+ * clear screen and send message to screen.  `message` will be truncated if it is longer than 16 characters
+ */
+  function staticMessage(message) {
+  	lcd.clear();
+  	lcd.message(message);
+  }
+
+/*
+  scroll `message` across the display
+  */
+  var scroller;
+  function scrollMessage(message) {
+      var screenLen = 16;
+      clearInterval(scroller);
+      var messageLen = message.length;
+      var i = 0;
+      scroller = setInterval(function(){
+  	var substr;
+  	if (i < messageLen - screenLen) {
+  	    substr = message.substr(i, screenLen);
+  	} else if (i < messageLen) {
+  	    substr = message.substr(i, messageLen - i) + " " + message.substr(0,(screenLen - (messageLen - i -1)));
+  	} else {
+  	    i = 0;
+  	    substr = message.substr(i, screenLen);
+  	}
+  	lcd.clear();
+  	lcd.message(substr);
+  	i++;
+      },400);
+  }
+  /*
+    display a list of menu items associated with actions  that are invoked when the select button is pressed
+    `list` --
+    `property`
+  */
+  function displayMenu(list,property) {
+      var promise = new Promise(function(resolve, reject) {
+  	var curIndex = 0;
+  	var total = list.length;
+  	lcd.clear();
+  	staticMessage((curIndex +1) + " of " + total + "\n* " + list[curIndex][property].substr(0,13));
+  	var selectMenu = function(button) {
+  	    switch(button) {
+  	    case SELECT:
+  		lcd.removeListener("button_up", arguments.callee);
+  		resolve(list[curIndex]);
+  		break;
+  	    case SOUTH:
+  		if (curIndex < list.length-1)
+  		    curIndex++;
+  		break;
+  	    case NORTH:
+  		if (curIndex > 0)
+  		    curIndex--;
+  		break
+  	    }
+  	    lcd.clear();
+  	    staticMessage((curIndex +1) + " of " + total + "\n* " + list[curIndex][property].substr(0,13));
+  	}
+  	lcd.on('button_up',selectMenu);
+      });
+      return promise;
+  }
 
 
-module.exports = {'initialize':initialize,'staticMessage':staticMessage, 'displayMenu':displayMenu};
+  module.exports = {'initialize':initialize,'staticMessage':staticMessage, 'displayMenu':displayMenu};
